a40e8a9c4ea2d488ddbdc5b3931b37da2b0d10db
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-30 11:15:36 +0200

    created server with 404 not found

--- back/server.js
+++ back/server.js
@@ -1,1 +1,9 @@
 console.log("try to start a web server");
+
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server successfully started on port 3000");
+});
