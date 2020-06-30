06b216b87a827f7076ae777f02268e2db8f7963b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-30 11:20:26 +0200

    added first middleware

--- back/server.js
+++ back/server.js
@@ -4,6 +4,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res, next) => {
+  res.send("Coucou JL !");
+});
+
 app.listen(3000, () => {
   console.log("Server successfully started on port 3000");
 });
