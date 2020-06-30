930ca351ea8059b0e09fd8cac63fbef634cf08a7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-30 17:28:15 +0200

    hello world with timeout

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'front';
+  name = 'World';
+
+  constructor() {
+    setTimeout(() => {
+      this.name = 'coucou';
+    }, 2000);
+  }
 }
