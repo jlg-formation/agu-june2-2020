00909f0fc099b9353736494c27d9e2dc0f1973f5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-01 16:29:42 +0200

    added service and use of it

---
+++ front/src/app/services/article.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Article } from '../interfaces/article';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArticleService {
+  articles: Article[] = [
+    { name: 'Tournevis', price: 2.34, qty: 123 },
+    { name: 'Tournevis Cruciforme', price: 2.34, qty: 1000 },
+    { name: 'Pince', price: 12.34, qty: 45 },
+    { name: 'Scie', price: 34, qty: 12 },
+    { name: 'Tondeuse', price: 340, qty: 20 },
+  ];
+
+  constructor() {
+    console.log('service instantiated');
+  }
+}
