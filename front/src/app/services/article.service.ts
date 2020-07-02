87105d85457530ab67379d73f49a2595998ee170
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-02 13:56:39 +0200

    article does not init article list

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -14,13 +14,7 @@ export class ArticleService {
   getArticles(): Article[] {
     const str = localStorage.getItem('articles');
     if (!str) {
-      return [
-        { name: 'Tournevis', price: 2.34, qty: 123 },
-        { name: 'Tournevis Cruciforme', price: 2.34, qty: 1000 },
-        { name: 'Pince', price: 12.34, qty: 45 },
-        { name: 'Scie', price: 34, qty: 12 },
-        { name: 'Tondeuse', price: 340, qty: 20 },
-      ];
+      return [];
     }
     return JSON.parse(str) as Article[];
   }
