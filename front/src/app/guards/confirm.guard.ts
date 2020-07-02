fb68e699d57dfceda6768a558c90caf1687a2aec
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-02 15:12:01 +0200

    added guard for example

---
+++ front/src/app/guards/confirm.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfirmGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const answer = window.confirm('Are you sure ?');
+    return answer;
+  }
+}
--- front/src/app/stock/stock-routing.module.ts
+++ front/src/app/stock/stock-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { CreateComponent } from './create/create.component';
+// import { ConfirmGuard } from '../guards/confirm.guard';
 
 const routes: Routes = [
+  // { path: 'stock', component: ListComponent, canActivate: [ConfirmGuard] },
   { path: 'stock', component: ListComponent },
   { path: 'stock/new', component: CreateComponent },
 ];
