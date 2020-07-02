c7eb4ea43a06dfaf17cc4e5d204cc527837f2039
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-02 15:56:47 +0200

    ng test working but not covered

--- front/src/app/stock/list/list.component.spec.ts
+++ front/src/app/stock/list/list.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
+import { WidgetModule } from 'src/app/widget/widget.module';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -8,6 +9,7 @@ describe('ListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [WidgetModule],
       declarations: [ ListComponent ]
     })
     .compileComponents();
