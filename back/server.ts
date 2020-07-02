8735588f23511d35791507382307f6acd573dfd0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-02 13:56:09 +0200

    back-end get articles

--- back/server.ts
+++ back/server.ts
@@ -2,6 +2,14 @@ import express from "express";
 import serveIndex from "serve-index";
 import cors from "cors";
 
+const articles = [
+  { name: 'Tournevis', price: 2.34, qty: 123 },
+  { name: 'Tournevis Cruciforme', price: 2.34, qty: 1000 },
+  { name: 'Pince', price: 12.34, qty: 45 },
+  { name: 'Scie', price: 34, qty: 12 },
+  { name: 'Tondeuse', price: 340, qty: 20 },
+];
+
 const app = express();
 
 app.use(cors());
@@ -10,6 +18,10 @@ app.use(cors());
 app.use((req, res, next) => {
   console.log("req.url", req.url);
   next();
+});
+
+app.get('/ws/articles', (req, res) => {
+  res.json(articles);
 });
 
 app.use(express.static("www"));
