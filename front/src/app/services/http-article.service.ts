10b678d5f99f5f0ad51cbcaa2bf33ec2b23494a1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-02 13:57:48 +0200

    sync article list
    when refreshing, the article list is updated from back-end.

--- front/src/app/services/http-article.service.ts
+++ front/src/app/services/http-article.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { ArticleService } from './article.service';
+import { Article } from '../interfaces/article';
 
 @Injectable({
   providedIn: 'root',
@@ -15,9 +16,11 @@ export class HttpArticleService extends ArticleService {
 
   refresh() {
     console.log('start refresh');
-    this.http.get('http://localhost:3000/ws/articles').subscribe({
-      next: (data) => {
-        console.log('data: ', data);
+    this.http.get<Article[]>('http://localhost:3000/ws/articles').subscribe({
+      next: (articles) => {
+        console.log('articles: ', articles);
+        this.articles = articles;
+        this.save();
       },
       error: (err) => {
         console.error('err: ', err);
