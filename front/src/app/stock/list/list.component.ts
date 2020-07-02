7dd11e94f0850b56c847f2b537645768308e50fd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-02 10:13:47 +0200

    added toggle method

--- front/src/app/stock/list/list.component.ts
+++ front/src/app/stock/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleService } from 'src/app/services/article.service';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   selector: 'app-list',
@@ -10,4 +11,8 @@ export class ListComponent implements OnInit {
   constructor(public articleService: ArticleService) {}
 
   ngOnInit(): void {}
+
+  toggle(article: Article) {
+    console.log('toggle', article);
+  }
 }
