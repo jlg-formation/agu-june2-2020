728595d8e0c5b6a6312f9bbdb73feec510038d48
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-03 17:28:25 +0200

    added code outside angular zone

--- front/src/app/widget/chrono/chrono.component.ts
+++ front/src/app/widget/chrono/chrono.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, ChangeDetectorRef } from '@angular/core';
 import { interval, BehaviorSubject, Subscription } from 'rxjs';
 import { map, startWith, filter } from 'rxjs/operators';
 import { Router, NavigationStart } from '@angular/router';
@@ -13,29 +13,44 @@ export class ChronoComponent implements OnInit {
   durationRoute$ = new BehaviorSubject<number>(0);
   subscription: Subscription;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private ngZone: NgZone,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    interval(1000)
-      .pipe(
-        map((n) => n + 1),
-        startWith(0)
-      )
-      .subscribe(this.duration$);
+    this.ngZone.runOutsideAngular(() => {
+      console.log('start observable');
+      interval(1000)
+        .pipe(
+          map((n) => n + 1),
+          startWith(0)
+        )
+        .subscribe((n) => {
+          this.duration$.next(n);
+          this.changeDetectorRef.detectChanges();
+        });
+    });
 
     this.router.events
       .pipe(filter((event) => event instanceof NavigationStart))
       .subscribe({
         next: (event) => {
-          if (this.subscription) {
-            this.subscription.unsubscribe();
-          }
-          this.subscription = interval(1000)
-            .pipe(
-              map((n) => n + 1),
-              startWith(0)
-            )
-            .subscribe(this.durationRoute$);
+          this.ngZone.runOutsideAngular(() => {
+            if (this.subscription) {
+              this.subscription.unsubscribe();
+            }
+            this.subscription = interval(1000)
+              .pipe(
+                map((n) => n + 1),
+                startWith(0)
+              )
+              .subscribe((n) => {
+                this.durationRoute$.next(n);
+                this.changeDetectorRef.detectChanges();
+              });
+          });
         },
       });
   }
