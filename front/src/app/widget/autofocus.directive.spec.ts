f411129bc61f7da3349f2f5198280d069ad478cf
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-03 12:04:52 +0200

    added unit test on directive

--- front/src/app/widget/autofocus.directive.spec.ts
+++ front/src/app/widget/autofocus.directive.spec.ts
@@ -1,8 +1,37 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AutofocusDirective } from './autofocus.directive';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: '<input type="text" appAutofocus>',
+})
+export class TestComponent {}
 
 describe('AutofocusDirective', () => {
-  // it('should create an instance', () => {
-  //   const directive = new AutofocusDirective();
-  //   expect(directive).toBeTruthy();
-  // });
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TestComponent, AutofocusDirective ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    // focus ?
+    const compiled = fixture.nativeElement;
+    const input = compiled.querySelector('input');
+    expect(input === document.activeElement).toBeTrue();
+  });
+
 });
+
